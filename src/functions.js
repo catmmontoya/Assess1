0c9123aa65b199097a427a05abf538b239b37a07
Caitlin Baldini <cat@listoglobal.com>
2024-01-14 15:40:18 -0700

    fixed stickycase

--- src/functions.js
+++ src/functions.js
@@ -66,9 +66,9 @@ function stickyCase(string) {
   let newString = "";
   for (let i = 0; i < string.length; i++) {
     if (i % 2 === 0) {
-      newString += string[i].toUpperCase();
-    } else {
       newString += string[i];
+    } else {
+      newString += string[i].toUpperCase();
     }
   }
   return newString;
